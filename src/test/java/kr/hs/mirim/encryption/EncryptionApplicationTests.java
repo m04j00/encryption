114062ce5b073f12997e62c84a9be00f757770e7
m04j00 <m04j00@gmail.com>
2022-03-30 12:07:42 +0900

    Initial commit

---
+++ src/test/java/kr/hs/mirim/encryption/EncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.mirim.encryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
